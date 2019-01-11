762f74328ac87c8cc87d54a891ee0666d79bae4a
Teiluja_Snamor <138692@student.asd20.org>
2019-01-11 12:58:31 -0700

    Stack

---
+++ SemesterTwo/Stack/Stack/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            Stack myStack = new Stack();
+
+            while (input != 5)
+            {
+                Console.WriteLine("1. Push a value to the top of the stack");
+                Console.WriteLine("2. Peek at the top value of the stack");
+                Console.WriteLine("3. Pop the top value from the stack");
+                Console.WriteLine("4. Prints the stack");
+                Console.WriteLine("5. Exit the program.");
+
+                input = int.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+
+                    int input2;
+                    Console.WriteLine(" ");
+                    Console.Write("What number would you like to add?: ");
+                    input2 = int.Parse(Console.ReadLine());
+
+                    myStack.Push(input2);
+                    Console.WriteLine(" ");
+
+                }
+
+                if (input == 2)
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine("SPOOKY!!! " + myStack.Peek() + " is your peeked number.");
+                    Console.WriteLine(" ");
+                    //myStack.Peek();
+                   
+                }
+
+                if (input == 3)
+                {
+                    myStack.Pop();
+                }
+
+                if (input == 4)
+                {
+                    myStack.Print();
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ SemesterTwo/Stack/Stack/Stack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Stack
+{
+    class Stack
+    {
+        List<int> holder = new List<int>();
+
+        public Stack()
+        {
+
+        }
+
+        //push
+        public void Push(int val)
+        {
+            holder.Add(val);
+        }
+        
+        //pop
+        public int Pop()
+        {
+            int first;
+            first = holder.Count - 1;
+            Console.WriteLine(" ");
+            Console.WriteLine(holder[holder.Count - 1] + " has been popped!");
+            Console.WriteLine(" ");
+            holder.RemoveAt(first);
+            return first;
+        }
+
+        //peek
+        public int Peek()
+        {
+           return holder[holder.Count - 1];
+            
+            
+        }
+
+        //print
+        public void Print()
+        {
+            for (int i = 0; i < holder.Count; i++)
+            {
+
+                Console.WriteLine(" ");
+                Console.Write(holder[i]);
+               
+
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+        }
+    }
+}
